9e0693a1feab948063c01a2130269fda9b305618
PaulSPuntusov <pps_ru@mail.ru>
2024-08-01 00:03:58 +0300

    Incapsulations 3. Student вроде, сделал..

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Student st1 = new Student;
-        st1.setName("Petya");
+        int[] grades = {5,5,5};
+        Student st1 = new Student("Petya",grades);
+        st1.setGrade(5);
+        st1.setGrade(5);
         System.out.println(st1.toString());
+        Student st2 = new Student("Vasya");
+        System.out.println(st2.toString());
+
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -2,21 +2,28 @@ import java.util.Arrays;
 
 public class Student {
     String name;
-    private int[] grades = null;
+    private int[] grades = {0};
+    public Student(String name){
+        this.name = name;
+    }
 
-    private Student(String name, int[] grades) {
+    public Student(String name, int... grades) {
+        this.name = name;
         gradesCheck(grades);
-        this.grades = grades;
+        if(this.grades[0]==0) {
+            this.grades = grades;
+        } else
+        {
+            int oldGradesLength = this.grades.length;
+            this.grades = Arrays.copyOf(this.grades, this.grades.length + grades.length);
+            for(int i= oldGradesLength; i<this.grades.length;i++){
+                this.grades[i] = grades[i-oldGradesLength];
+            }
+        }
     }
 
-    private Student(String name) {
-        this.name = name;
-        this.grades = null;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+
 
     public void gradesCheck(int[] grades) {
         for (int i = 0; i < grades.length; i++) {
@@ -24,6 +31,10 @@ public class Student {
                 throw new IllegalArgumentException("Оценки должны быть в диапазоне 2-5");
             }
         }
+    }
+
+    public int[] getGrades() {
+        return Arrays.copyOf(grades,grades.length);
     }
 
     public void gradeCheck(int grade) {
@@ -34,15 +45,19 @@ public class Student {
 
     public void setGrade(int grade) {
         gradeCheck(grade);
-        this.grades = Arrays.copyOf(this.grades,this.grades.length+1);
-        grades[grades.length-1] = grade;
+        if (grades[0] == 0) {
+            grades[0] = grade;
+        } else {
+            this.grades = Arrays.copyOf(this.grades, this.grades.length + 1);
+            grades[grades.length - 1] = grade;
+        }
     }
 
     @Override
     public String toString() {
         return "Student{" +
-                "Имя='" + name +
-                "оценки"+ Arrays.toString(grades)+
+                "Имя='" + name + "'/" +
+                " оценки " + Arrays.toString(grades) +
                 '}';
     }
 }
