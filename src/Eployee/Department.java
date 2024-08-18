a39ce66de995214dda712feeb8c12854994c6e69
PaulSPuntusov <pps_ru@mail.ru>
2024-08-18 19:55:16 +0300

    Incapsulations 4. Employee-modified я не понял, что здесь требуется упростить, но пакетный лоступ добавил

---
+++ src/Eployee/Department.java
@@ -0,0 +1,39 @@
+public class Department {
+    private String depName;
+    private Employee boss;
+
+    public Department(String depName, Employee boss) {
+        this.depName = depName;
+        if ((boss != null) && (boss.getDep() != this)) {
+            bossDepCheck(boss);
+        }
+        this.boss = boss;
+    }
+
+    public Department(String depName) {
+        this(depName, null);
+    }
+
+    public Employee getBoss() {
+        return boss;
+    }
+
+    public String getDepName() {
+        return depName;
+    }
+
+    public void setDepName(String depName) {
+        this.depName = depName;
+    }
+
+    public void bossDepCheck(Employee boss) {
+        if (boss.getDep() != this) {
+            boss.setDep(this);
+        }
+    }
+
+    public void setBoss(Employee boss) {
+        bossDepCheck(boss);
+        this.boss = boss;
+    }
+}
